3a83b79fddc23c5d43b6da64f7ebcf0edfdc922a
Daniel Eros <drerosdaniel@gmail.com>
2017-05-17 22:19:04 +0200

    implemented tag question

--- app_logic.py
+++ app_logic.py
@@ -29,7 +29,9 @@ def display_question(id, count_view=True):
     view_number = question_list[0][2] + 1
     ui.update_record('question', "view_number=%s" % (view_number), "id=%s" % (id))
     answer_list = ui.get_record_from_sql_db('answer', "question_id=%s" % (id))
-    return render_template('display_question.html', id=id, title=title, message=message, list_answers=answer_list)# view counter += 1
+    # SELECT tag.name FROM tag JOIN question_tag ON question_tag.tag_id=tag.id WHERE question_tag.question_id=1;
+    tag_list = ui.get_record_from_tag('tag', 'question_tag ON question_tag.tag_id=tag.id', "question_tag.question_id=%s" % (id))
+    return render_template('display_question.html', id=id, title=title, message=message, list_answers=answer_list, tag_list=tag_list)
 
 
 @app.route('/question/<question_id>/new-answer')
@@ -88,3 +90,29 @@ def delete_question(question_id):
     ui.delete_record('comment', "question_id=%s" % (question_id))
     ui.delete_record('question', "id=%s" % (question_id))
     return redirect('/')
+
+
+@app.route('/question/<question_id>/new-tag', methods=['POST'])
+def add_tag(question_id):
+    existing_tags = ui.get_existing_tags()
+    return render_template("add_tag.html", question_id=question_id, existing_tags=existing_tags)
+
+
+@app.route('/question/<question_id>/add_new_tag', methods=['POST'])
+def add_new_tag(question_id):
+    if request.form['new_tag']:
+        # insert a new tag into tag if it is not in it
+        if not ui.get_record_from_sql_db('tag', "name='%s'" % (request.form['new_tag'])):
+            ui.add_item_to_tag('tag', request.form['new_tag'])
+        # get new tag's id
+        new_tag_id_list = ui.get_tag_id_by_name(request.form['new_tag'])
+        # add this to question_tag if it is not added yet
+        if not ui.get_record_from_sql_db('question_tag', "question_id=%s AND tag_id=%s" % (question_id, new_tag_id_list[0][0])):
+            ui.add_item_to_question_tag('question_tag', question_id, new_tag_id_list[0][0])
+    else:
+        # get the tag id of the existing tag
+        existing_tag_id_list = ui.get_tag_id_by_name(request.form['existing_tag'])
+        # if question_id, existing_tag_id_list[0][0] is NOT in question_tag table
+        if not ui.get_record_from_sql_db('question_tag', "question_id=%s AND tag_id=%s" % (question_id, existing_tag_id_list[0][0])):
+            ui.add_item_to_question_tag('question_tag', question_id, existing_tag_id_list[0][0])
+    return redirect('/question/' + question_id)
--- ui.py
+++ ui.py
@@ -13,16 +13,45 @@ def get_record_from_sql_db(table_name, condition):
     return result
 
 
+def get_record_from_tag(table_name, join_text, condition):
+    # SELECT tag.name FROM tag JOIN question_tag ON question_tag.tag_id=tag.id WHERE question_tag.question_id=1;
+    result = handle_database("""SELECT name FROM {} JOIN {} WHERE {};""".format(table_name, join_text, condition))
+    return result
+
+
+def get_existing_tags():
+    result = handle_database("""SELECT name FROM tag;""")
+    return result
+
+
+def get_tag_id_by_name(tag_name):
+    result = handle_database("""SELECT id FROM tag WHERE name='{}';""".format(tag_name))
+    return result
+
+
 def add_item_to_sql_db(table, request):
     handle_database("""INSERT INTO {} ({}, {}, {}, {}, {}) VALUES ('{}', {}, {}, '{}', '{}');""".format(table, 
-         'submission_time', 'view_number', 'vote_number', 'title', 'message',
-         str(datetime.now())[:-7], 0, 0, request['new_question_title'], request['new_question_message']))
+                    'submission_time', 'view_number', 'vote_number', 'title', 'message',
+                    str(datetime.now())[:-7], 0, 0, request['new_question_title'], request['new_question_message']))
 
 
 def add_item_to_answer_db(table, request):
-    handle_database("""INSERT INTO {} ({}, {}, {}, {}) VALUES ('{}', {}, {}, '{}');""".format(table, 
-         'submission_time', 'vote_number', 'question_id', 'message',
-         str(datetime.now())[:-7], 0, request['question_id'], request['new_answer_message']))
+    handle_database("""INSERT INTO {} ({}, {}, {}, {}) VALUES ('{}', {}, {}, '{}');""".format(table,
+                    'submission_time', 'vote_number', 'question_id', 'message',
+                    str(datetime.now())[:-7], 0, request['question_id'], request['new_answer_message']))
+
+
+def add_item_to_question_tag(table, question_id, tag_id):
+    handle_database("""INSERT INTO {} ({}, {}) VALUES ({}, {});""".format(table,
+                    'question_id', 'tag_id',
+                    question_id, tag_id))
+
+
+def add_item_to_tag(table, name):
+    handle_database("""INSERT INTO {} ({}) VALUES ('{}');""".format(table,
+                    'name',
+                    name))
+
 
 def update_record(table_name, set_value, condition):
     handle_database("""UPDATE {} SET {} WHERE {}""".format(table_name, set_value, condition))
