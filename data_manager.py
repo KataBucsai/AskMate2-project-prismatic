ef03c6a7958f18958b4c56ff927cd6bdeaa806a5
Daniel Eros <drerosdaniel@gmail.com>
2017-05-17 15:10:00 +0200

    deleted csv handling function remnants

--- data_manager.py
+++ data_manager.py
@@ -5,7 +5,7 @@ import psycopg2
 import sys
 
 
-def init(command):
+def handle_database(command):
     try:
         connect_str = "dbname='eros' user='eros' host='localhost' password='titok'"
         conn = psycopg2.connect(connect_str)
@@ -24,72 +24,29 @@ def init(command):
 
 
 def get_table_from_sql_db(table_name, order_by=['submission_time', 'DESC']): 
-    result = init("""SELECT * FROM {} ORDER BY {} {} LIMIT 5;""".format(table_name, order_by[0], order_by[1]))
+    result = handle_database("""SELECT * FROM {} ORDER BY {} {} LIMIT 5;""".format(table_name, order_by[0], order_by[1]))
     return result
+
 
 def get_record_from_sql_db(table_name, condition):
-    result = init("""SELECT * FROM {} WHERE {};""".format(table_name, condition))
+    result = handle_database("""SELECT * FROM {} WHERE {};""".format(table_name, condition))
     return result
-
-
-# write a @table into a file
-#
-# @file_name: string
-# @table: list of lists of strings
-def write_table_to_file(file_name, table, indices):
-    table = base64_coder(table, indices)
-    with open(file_name, "w") as file:
-        for record in table:
-            row = ','.join(record)
-            file.write(row + "\n")
-
-
-def get_time_stamp():
-    return str(int(time.time()))
-
-
-def get_timeform_from_stamp(table):
-    for row in table:
-        row[1] = datetime.datetime.fromtimestamp(int(row[1])).strftime('%Y-%m-%d %H:%M:%S')
-    return table
 
 
 def add_item_to_sql_db(table, request):
-    init("""INSERT INTO {} ({}, {}, {}, {}, {}) VALUES ('{}', {}, {}, '{}', '{}');""".format(table, 
+    handle_database("""INSERT INTO {} ({}, {}, {}, {}, {}) VALUES ('{}', {}, {}, '{}', '{}');""".format(table, 
          'submission_time', 'view_number', 'vote_number', 'title', 'message',
          str(datetime.now())[:-7], 0, 0, request['new_question_title'], request['new_question_message']))
 
 
 def add_item_to_answer_db(table, request):
-    init("""INSERT INTO {} ({}, {}, {}, {}) VALUES ('{}', {}, {}, '{}');""".format(table, 
+    handle_database("""INSERT INTO {} ({}, {}, {}, {}) VALUES ('{}', {}, {}, '{}');""".format(table, 
          'submission_time', 'vote_number', 'question_id', 'message',
          str(datetime.now())[:-7], 0, request['question_id'], request['new_answer_message']))
 
 def update_record(table_name, set_value, condition):
-    init("""UPDATE {} SET {} WHERE {}""".format(table_name, set_value, condition))
-
-
-def add_item_to_answer_table(table, request):
-    max_id = 0
-    if len(table) > 0:
-        max_id = max(int(i[0]) for i in table)
-    table.append([str(max_id+1),
-                 get_time_stamp(),
-                 '0',
-                 request['question_id'],
-                 request['new_answer_message'],
-                 ''])
-    return table
+    handle_database("""UPDATE {} SET {} WHERE {}""".format(table_name, set_value, condition))
 
 
 def delete_record(table_name, condition):
-    print("""DELETE FROM {} WHERE {};""".format(table_name, condition))
-    init("""DELETE FROM {} WHERE {};""".format(table_name, condition))
-    '''for row_id, row_value in enumerate(list_table):
-        if row_value[0] == question_id:
-            del(list_table[row_id])
-            break
-    return list_table'''
-
-
-
+    handle_database("""DELETE FROM {} WHERE {};""".format(table_name, condition))
